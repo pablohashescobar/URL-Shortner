9df5302c71085ba80fc0f2b1e6fb76a5c6a61040
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-05-29 16:49:26 +0530

    initial commit and user schema created

---
+++ lib/url_shortener_web/views/user_view.ex
@@ -0,0 +1,18 @@
+defmodule UrlShortenerWeb.UserView do
+  use UrlShortenerWeb, :view
+  alias UrlShortenerWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      email: user.email,
+      encrypted_password: user.encrypted_password}
+  end
+end
